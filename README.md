19823386cb21c9df9a27e661cda28d64dafe0336
Lutz Althüser <coding@l-althueser.de>
2018-08-31 21:44:35 +0200

    v0.1.2

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # Index for Jupyter notebooks
+`nbindex` is a small package containing several Javascript additions for Jupyter
+notebooks like a Table of Content or a Code Hider. These additions are designed
+to be executed in a notebook cell so that the added HTML and Javascript functions
+will remain hidden in the cell output and can be exported by the regular HTML
+export of Jupyter.
 
 Installation:
 
     pip install nbindex-jupyter
 
-Usage
+## Usage
 
-    from nbindex import nbindex
+### ToC and CodeHider
 
+    from nbindex import utils, floating
+    floating.tableofcontent()
+    floating.codehider()
 
+![toc_codehider.png][examples/toc_codehider.png]
+
+### Numbered objects (Figures, ...)
+
+    from nbindex import numbered
+    numbered.Figure()
+    numbered.object("Figure")
+
+![numbered.png][examples/numbered.png]
--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 # encoding: utf-8
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='nbindex-jupyter',
-    version='0.1.1',
+    version='0.1.2',
     packages=['nbindex'],
     url='https://github.com/l-althueser/nbindex-jupyter',
     license='BSD 3-Clause License',
     author='Lutz Althüser',
     author_email='coding@l-althueser.de',
-    description='Javascript based Jupyter Notebook additions (Table of Content, hide code, Figure numbers, ...)'
+    description='Javascript based Jupyter Notebook additions (Table of Content, hide code, Figure numbers, ...)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: IPython",
+        "Framework :: Jupyter",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
 )
